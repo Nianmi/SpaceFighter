49caee83f9335d3bac94d934a690214fc1dc4ff5
Nianmi <carlo.weidinger@gmail.com>
2016-01-27 03:23:02 +0100

    Started on part 2 off the tutorial
    Added Enemies skript.
    Added Parts of the Collision Detection.

--- SpaceFighter/SpaceFighter/EntityManager.cs
+++ SpaceFighter/SpaceFighter/EntityManager.cs
@@ -16,11 +16,12 @@ namespace SpaceFighter
 	{
 		static List<Entity> entities = new List<Entity>();
 		static List<Bullet> bullets = new List<Bullet>();
+        static List<Enemy> enemies = new List<Enemy>();
 
-		static bool isUpdating;
+        static bool isUpdating;
 		static List<Entity> addedEntities = new List<Entity>();
 
-		public static int Count { get { return entities.Count; } }
+        public static int Count { get { return entities.Count; } }
 
 		public static void Add(Entity entity)
 		{
@@ -35,7 +36,9 @@ namespace SpaceFighter
 			entities.Add(entity);
 			if (entity is Bullet)
 				bullets.Add(entity as Bullet);
-		}
+            else if (entity is Enemy)
+                enemies.Add(entity as Enemy);
+        }
 
 		public static void Update()
 		{
@@ -53,9 +56,16 @@ namespace SpaceFighter
 
 			entities = entities.Where(x => !x.IsExpired).ToList();
 			bullets = bullets.Where(x => !x.IsExpired).ToList();
-		}
+            enemies = enemies.Where(x => !x.IsExpired).ToList();
+        }
 
-		public static void Draw(SpriteBatch spriteBatch)
+        private static bool IsColliding(Entity a, Entity b)
+        {
+            float radius = a.Radius + b.Radius;
+            return !a.IsExpired && !b.IsExpired && Vector2.DistanceSquared(a.Position, b.Position) < radius * radius;
+        }
+
+        public static void Draw(SpriteBatch spriteBatch)
 		{
 			foreach (var entity in entities)
 				entity.Draw(spriteBatch);
